c472a30ece86a8438914df2cd1fc98f4ba282f8c
musowe <envoymalumbemusowe@gmail.com>
2023-09-27 17:09:22 -0700

    last task puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash 
+# using puppet manifest to make changes to our configuration file
+
+include stdlib
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
+}
