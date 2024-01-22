d3f861991d83b1994e1e23058dfee6f99ad4b34f
musowe <envoymalumbemusowe@gmail.com>
2024-01-21 23:57:48 -0800

    advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""This script api Function to query a list of all hot posts on a given Reddit subreddit."""
+"""Contains recurse function"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """The function Returns a list of titles of all hot posts on a given subreddit."""
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "after": after,
